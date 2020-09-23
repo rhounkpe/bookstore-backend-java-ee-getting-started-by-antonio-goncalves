7ea0e61a173688db80f150cc2ee89b71ac0d9353
Rufin Hounkpe <rhounkpe@gmail.com>
2020-09-23 14:15:31 +0200

    Implementing validation

---
+++ src/main/java/com/pluralsight/bookstore/repository/BookRepositoryTest.java
@@ -0,0 +1,19 @@
+package com.pluralsight.bookstore.repository;
+
+import com.pluralsight.bookstore.model.Book;
+import com.pluralsight.bookstore.model.Language;
+
+
+// @RunWith(Arquillian.class)
+public class BookRepositoryTest {
+    /*@Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addClass(BookRepository.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+    @Test
+    public void create() {
+    }*/
+}
