c3b794aefa51de95aa92d0d03f18584af0cc4958
Rufin Hounkpe <rhounkpe@gmail.com>
2020-09-23 20:53:41 +0200

    Implementing the Book Endpoint: Get One

--- src/main/java/com/pluralsight/bookstore/rest/BookEndpoint.java
+++ src/main/java/com/pluralsight/bookstore/rest/BookEndpoint.java
@@ -4,8 +4,10 @@ import com.pluralsight.bookstore.model.Book;
 import com.pluralsight.bookstore.repository.BookRepository;
 
 import javax.inject.Inject;
+import javax.validation.constraints.Min;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import java.util.List;
@@ -17,9 +19,20 @@ public class BookEndpoint {
     @Inject
     private BookRepository bookRepository;
 
-    public Book getBook(Long id) {
-        return bookRepository.find(id);
+    @GET
+    @Path("/{id : \\d+}")
+    @Produces(APPLICATION_JSON)
+    public Response getBook(@PathParam("id") @Min(1) Long id) {
+
+        Book book = bookRepository.find(id);
+
+        if (book == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        return Response.ok(book).build();
     }
+
 
     public Book createBook(Book book) {
         return bookRepository.create(book);
